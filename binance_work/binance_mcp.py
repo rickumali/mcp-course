9f72007202e2c552df2c061aedbaeee315fb414a
Rick Umali <rickumali@gmail.com>
2026-02-16 22:25:30 -0500

    Adds get_price_price_change function
    Per peer-review, I learn about:
    
        return response.json()["priceChange"]

--- binance_work/binance_mcp.py
+++ binance_work/binance_mcp.py
@@ -32,5 +32,23 @@ def get_price(symbol: str) -> Any:
     return response.json()
 
 
+@mcp.tool()
+def get_price_price_change(symbol: str) -> Any:
+    """
+    Get price change of a crypto asset for the last 24 hours from Binance
+
+    Args:
+        symbol (str): The symbol of the crypto asset
+
+    Returns:
+        Any: The price change of the crypto asset for last 24 hours
+    """
+    symbol = get_symbol_from_name(symbol)
+    url = f"https://api.binance.us/api/v3/ticker/24hr?symbol={symbol}"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()["priceChange"]
+
+
 if __name__ == "__main__":
     mcp.run(transport="stdio")
